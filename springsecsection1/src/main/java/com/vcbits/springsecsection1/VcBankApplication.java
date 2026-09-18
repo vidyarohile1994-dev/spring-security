e20c7a49b72e0fadc3ed422fca8d2ea113665a6b
Vidya Chand <vidyarohile1994@gmail.com>
2026-09-18 14:32:55 +0530

    First commit for spring security

---
+++ springsecsection1/src/main/java/com/vcbits/springsecsection1/VcBankApplication.java
@@ -0,0 +1,13 @@
+package com.vcbits.springsecsection1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VcBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VcBankApplication.class, args);
+	}
+
+}
